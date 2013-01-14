da547241b092487c7df42bc9cee3c0a9ea4e6244
Sébastien Lavoie <github@lavoie.sl>
2013-01-14 09:30:31 -0500

    first commit

---
+++ test/Test/Test/Index/Bar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Test\Test\Index;
+
+use Test\Index\Bar;
+
+class BarTest extends \PHPUnit_Framework_TestCase
+{
+    function initTest()
+    {
+        $obj = new Bar();
+    }
+}
---
+++ test/Test/Test/Other/Foo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Test\Test\Other;
+
+use Test\Other\Foo;
+
+class FooTest extends \PHPUnit_Framework_TestCase
+{
+    function initTest()
+    {
+        $obj = new Foo();
+    }
+}
